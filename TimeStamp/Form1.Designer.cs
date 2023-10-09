00b1318e87fee202a0eb5851fbe7dff139fc7b4a
unknown <ph_admin@SLH-PC-00050NEW.slh.corp>
2023-10-09 10:56:40 +0200

    ini commit

---
+++ TimeStamp/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TimeStamp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.buttonSet = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelitem = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "HH:mm:ss tt";
+            this.dateTimePicker2.Location = new System.Drawing.Point(8, 9);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(201, 20);
+            this.dateTimePicker2.TabIndex = 1;
+            this.dateTimePicker2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDownDO);
+            // 
+            // buttonSet
+            // 
+            this.buttonSet.Font = new System.Drawing.Font("Arial Rounded MT Bold", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSet.Location = new System.Drawing.Point(314, 9);
+            this.buttonSet.Name = "buttonSet";
+            this.buttonSet.Size = new System.Drawing.Size(59, 20);
+            this.buttonSet.TabIndex = 2;
+            this.buttonSet.Text = "Set";
+            this.buttonSet.UseVisualStyleBackColor = true;
+            this.buttonSet.Click += new System.EventHandler(this.buttonSet_Click);
+            this.buttonSet.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDownDO);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.labelitem);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.dateTimePicker2);
+            this.panel1.Controls.Add(this.buttonSet);
+            this.panel1.Location = new System.Drawing.Point(2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(392, 51);
+            this.panel1.TabIndex = 3;
+            // 
+            // labelitem
+            // 
+            this.labelitem.AutoSize = true;
+            this.labelitem.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelitem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.labelitem.Location = new System.Drawing.Point(8, 32);
+            this.labelitem.Name = "labelitem";
+            this.labelitem.Size = new System.Drawing.Size(0, 15);
+            this.labelitem.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(375, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.labelClose_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "HH:mm:ss tt";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(215, 9);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(92, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDownDO);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(398, 57);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TimeStamp";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDownDO);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button buttonSet;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelitem;
+    }
+}
+
---
+++ TimeStamp/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TimeStamp
+{
+    public partial class Form1 : Form
+    {
+        string itemPath;
+
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            InitializeForm(args);
+        }
+
+        // Initialize the form based on command line arguments
+        private void InitializeForm(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                itemPath = args[0];
+                labelitem.Text = itemPath;
+                ToolTip tooltip = new ToolTip();
+                tooltip.SetToolTip(labelitem, itemPath);
+            }
+            else
+            {
+                // Display an error message if no command line parameters are provided
+                MessageBox.Show("Missing command line parameters\nPlease start with TimeStamp.exe \"c:\\example\\Program.exe\"");
+                Environment.Exit(0);
+            }
+        }
+        // Handle key events, including Enter and Escape keys
+        private void KeyDownDO(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return)
+            {
+                CheckItem();
+            }
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                // Confirm exit when Escape key is pressed
+                if (MessageBox.Show("Exit TimeStamp ?", "Close ?", MessageBoxButtons.YesNo) == DialogResult.Yes) this.Close();
+            }
+        }
+        // Handle the click event of the "Set" button
+        private void buttonSet_Click(object sender, EventArgs e)
+        {
+            CheckItem();
+        }
+        // Check if the specified path is a file or a directory
+        private void CheckItem()
+        {
+            if (Directory.Exists(itemPath) || File.Exists(itemPath))
+            {
+                SetTime(File.Exists(itemPath));
+            }
+            else
+            {
+                // Display an error message if neither a folder nor a file could be found
+                MessageBox.Show($"Neither a folder nor a file could be found\n{itemPath}");
+            }
+            Environment.Exit(0);
+        }
+
+        // Set the last write time of the specified file or directory
+        private void SetTime(bool isFile)
+        {
+            try
+            {
+                DateTime newTime = new DateTime(dateTimePicker2.Value.Year, dateTimePicker2.Value.Month, dateTimePicker2.Value.Day, dateTimePicker1.Value.Hour, dateTimePicker1.Value.Minute, dateTimePicker1.Value.Second);
+
+                if (isFile)
+                {
+                    File.SetLastWriteTime(itemPath, newTime);
+                }
+                else
+                {
+                    Directory.SetLastWriteTime(itemPath, newTime);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Error: " + ex.ToString());
+            }
+        }
+        // Handle the click event of the close button
+        private void labelClose_Click(object sender, EventArgs e)
+        {
+            // Confirm exit when the close button is clicked
+            if (MessageBox.Show("Exit TimeStamp ?", "Close ?", MessageBoxButtons.YesNo) == DialogResult.Yes) Environment.Exit(0);     
+        }
+        
+    }
+}
